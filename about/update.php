1aa8389778d6e3ed21aa9a67389325a84b77e685
Md. Fawjul Azim <mdfawjulazim617@gmail.com>
2024-09-14 00:28:44 +0600

    about update section update

--- about/update.php
+++ about/update.php
@@ -5,7 +5,7 @@ require '../database.php';
 $id=$_POST['id'];
 $designation = $_POST['designation'];
 $name = $_POST['name'];
-$desp = $_POST['desp'];
+$desp = mysqli_real_escape_string($_POST['desp'],$db_connection);
 
 $update = "UPDATE abouts SET designation='$designation', name='$name', desp='$desp'";
 mysqli_query($db_connection,$update);
